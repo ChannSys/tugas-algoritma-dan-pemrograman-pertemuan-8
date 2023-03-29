b7a17678405af4418f3c528b30d5fa8001d89c82
Helga Chandra Eka Adhitia <128583475+ChannSys@users.noreply.github.com>
2023-03-29 09:42:56 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+// Soal Nomor 3 a
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Masukan Nilai N : ";
+    cin >> n;
+    
+    if (n > 50) {
+        n = n + 25;
+        cout << "N = " << n << endl;
+    } else {
+        n = n + 10;
+        cout << "N = " << n << endl;
+    }
+    
+    return 0;
+}
+
+// Soal Nomor 3 b
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Masukan Nilai N : ";
+    cin >> n;
+    
+    if (n > 50) {
+        n = n + 10;
+        cout << "N = " << n << endl;
+    } else {
+        n = n + 10;
+        cout << "N = " << n << endl;
+    }
+    
+    return 0;
+}
